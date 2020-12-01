d52e6833a0e46fc6422e4fac5eeef8c4b75ea6cf
Norbert Harangozó <maszek.solutions@gmail.com>
2020-12-01 02:30:53 +0100

    [dao] Add a MySQL implementation of LaptopDAO.
    Also added some tests to see if our methods are working.

---
+++ dao/src/main/java/hu/maszeksolutions/LaptopRegistry/dao/LaptopDAOMySQL.java
@@ -0,0 +1,123 @@
+package hu.maszeksolutions.LaptopRegistry.dao;
+
+import hu.maszeksolutions.LaptopRegistry.model.Color;
+import hu.maszeksolutions.LaptopRegistry.model.Laptop;
+import hu.maszeksolutions.LaptopRegistry.model.Manufacturer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.Collection;
+
+public class LaptopDAOMySQL implements LaptopDAO
+{
+    private static SessionFactory factory;
+
+    public LaptopDAOMySQL()
+    {
+        Configuration cfg = new Configuration();
+
+        try
+        {
+            factory = cfg.configure().buildSessionFactory();
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Database configuration process has failed. Are the credentials in your hibernate.properties file valid?");
+        }
+    }
+
+    @Override
+    public void createLaptop(Laptop laptop)
+    {
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(laptop);
+        tx.commit();
+        session.close();
+    }
+
+    @Override
+    public Laptop readLaptop(String serialNumber)
+    {
+        Session session = factory.openSession();
+        Laptop result = session.get(Laptop.class, serialNumber);
+        session.close();
+
+        return result;
+    }
+
+    @Override
+    public Collection<Laptop> readAllLaptops()
+    {
+        Session session = factory.openSession();
+        Collection<Laptop> result = session.createQuery("FROM Laptop").list();
+        session.close();
+
+        return result;
+    }
+
+    @Override
+    public void updateLaptop(Laptop laptop)
+    {
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.update(laptop);
+        tx.commit();
+        session.close();
+    }
+
+    @Override
+    public void deleteLaptop(Laptop laptop)
+    {
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.delete(laptop);
+        tx.commit();
+        session.close();
+    }
+
+    @Override
+    public Collection<Laptop> readLaptopsByColor(Color color)
+    {
+        Session session = factory.openSession();
+
+        Query query = session.createQuery("FROM Laptop WHERE color = :color");
+        query.setParameter("color", color);
+        Collection<Laptop> result = query.list();
+
+        session.close();
+
+        return result;
+    }
+
+    @Override
+    public Collection<Laptop> readLaptopsByManufacturer(Manufacturer manufacturer)
+    {
+        Session session = factory.openSession();
+
+        Query query = session.createQuery("FROM Laptop WHERE manufacturer = :manufacturer");
+        query.setParameter("manufacturer", manufacturer);
+        Collection<Laptop> result = query.list();
+
+        session.close();
+
+        return result;
+    }
+
+    @Override
+    public Collection<Laptop> readPremiumLaptops()
+    {
+        Session session = factory.openSession();
+
+        String hql = "FROM Laptop WHERE panel_type = 'IPS' AND resolutionX >= 1920 AND resolutionY >= 1080 AND memory_size >= 8192 AND storage_type = 'SSD'";
+        Query query = session.createQuery(hql);
+        Collection<Laptop> result = query.list();
+
+        session.close();
+
+        return result;
+    }
+}
---
+++ dao/src/test/java/hu/maszeksolutions/LaptopRegistry/dao/LaptopDAOMySQLTest.java
@@ -0,0 +1,116 @@
+package hu.maszeksolutions.LaptopRegistry.dao;
+
+import hu.maszeksolutions.LaptopRegistry.exceptions.*;
+import hu.maszeksolutions.LaptopRegistry.model.*;
+import hu.maszeksolutions.LaptopRegistry.model.Color;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class LaptopDAOMySQLTest
+{
+    @Test
+    public void TestDBConnection()
+    {
+        LaptopDAO dao = new LaptopDAOMySQL();
+    }
+
+    @Test
+    public void CreateLaptopTest() throws InvalidManufactureDate, InvalidGPU, InvalidCPU, InvalidMemorySize,
+            InvalidScreenSize, InvalidRefreshRate, InvalidUSBPortCount, InvalidResolution, InvalidStorageSize, InvalidName
+    {
+        LaptopDAO dao = new LaptopDAOMySQL();
+        Laptop laptop = DummyLaptop();
+        dao.createLaptop(laptop);
+        System.out.print("New laptop has been added: " +dao.readLaptop(laptop.getSerialNumber()));
+    }
+
+    @Test
+    public void ReadLaptopTest() throws InvalidManufactureDate, InvalidGPU, InvalidCPU, InvalidMemorySize,
+            InvalidScreenSize, InvalidRefreshRate, InvalidUSBPortCount, InvalidResolution, InvalidStorageSize, InvalidName
+    {
+        LaptopDAO dao = new LaptopDAOMySQL();
+        Laptop laptop = DummyLaptop();
+        dao.createLaptop(laptop);
+        System.out.print(dao.readLaptop(laptop.getSerialNumber()));
+    }
+
+    @Test
+    public void ReadAllLaptopsTest()
+    {
+        LaptopDAO dao = new LaptopDAOMySQL();
+        System.out.print(dao.readAllLaptops());
+    }
+
+    @Test
+    public void UpdateLaptopTest() throws InvalidMemorySize, InvalidManufactureDate, InvalidGPU, InvalidCPU,
+            InvalidScreenSize, InvalidRefreshRate, InvalidUSBPortCount, InvalidResolution, InvalidStorageSize, InvalidName
+    {
+        LaptopDAO dao = new LaptopDAOMySQL();
+        Laptop laptop = DummyLaptop();
+        dao.createLaptop(laptop);
+        System.out.println("Before update: " +dao.readLaptop(laptop.getSerialNumber()));
+        laptop.setMemorySize(16384);
+        dao.updateLaptop(laptop);
+        System.out.print("After update: " +dao.readLaptop(laptop.getSerialNumber()));
+    }
+
+    @Test
+    public void DeleteLaptopTest() throws InvalidManufactureDate, InvalidGPU, InvalidCPU, InvalidMemorySize,
+            InvalidScreenSize, InvalidRefreshRate, InvalidUSBPortCount, InvalidResolution, InvalidStorageSize, InvalidName
+    {
+        LaptopDAO dao = new LaptopDAOMySQL();
+        Laptop laptop = DummyLaptop();
+        dao.createLaptop(laptop);
+        System.out.println("Before deletion: " +dao.readAllLaptops());
+        dao.deleteLaptop(laptop);
+        System.out.print("After deletion: " +dao.readAllLaptops());
+    }
+
+    @Test
+    public void ReadLaptopsByColorTest()
+    {
+        LaptopDAO dao = new LaptopDAOMySQL();
+        System.out.print(dao.readLaptopsByColor(Color.Black));
+    }
+
+    @Test
+    public void ReadLaptopsByManufacturerTest()
+    {
+        LaptopDAO dao = new LaptopDAOMySQL();
+        System.out.print(dao.readLaptopsByManufacturer(Manufacturer.Acer));
+    }
+
+    @Test
+    public void ReadPremiumLaptopsTest()
+    {
+        LaptopDAO dao = new LaptopDAOMySQL();
+        System.out.print(dao.readPremiumLaptops());
+    }
+
+    Laptop DummyLaptop() throws InvalidName, InvalidScreenSize, InvalidResolution, InvalidRefreshRate,
+            InvalidCPU, InvalidMemorySize, InvalidGPU, InvalidStorageSize, InvalidUSBPortCount, InvalidManufactureDate
+    {
+        Laptop laptop = new Laptop();
+
+        laptop.setManufacturer(Manufacturer.Acer);
+        laptop.setName("Aspire 5 A515-51G");
+        laptop.setColor(Color.Black);
+        laptop.setScreenSize(15.6);
+        laptop.setPanelType(PanelType.TN);
+        laptop.setResolutionX(1920);
+        laptop.setResolutionY(1080);
+        laptop.setRefreshRate(60);
+        laptop.setCpu("Intel Core i3-8130U");
+        laptop.setMemoryType(MemoryType.DDR4);
+        laptop.setMemorySize(8192);
+        laptop.setGpu("NVIDIA GeForce MX130");
+        laptop.setStorageType(StorageType.SSD);
+        laptop.setStorageSize(250);
+        laptop.setOpticalDrive(false);
+        laptop.setNumberOfUSBPorts(3);
+        laptop.setManufactureDate(LocalDate.of(2018, 11, 15));
+
+        return laptop;
+    }
+}
