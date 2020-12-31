9f479858360e894e4ac7e1fccb37dd3f8bcc365a
Norbert Harangozó <maszek.solutions@gmail.com>
2020-12-31 15:29:58 +0100

    [controller] Implement "show one laptop" REST functionality.

--- controller/src/main/java/hu/maszeksolutions/LaptopRegistry/controller/LaptopRestController.java
+++ controller/src/main/java/hu/maszeksolutions/LaptopRegistry/controller/LaptopRestController.java
@@ -1,9 +1,11 @@
 package hu.maszeksolutions.LaptopRegistry.controller;
 
+import hu.maszeksolutions.LaptopRegistry.exceptions.LaptopNotFound;
 import hu.maszeksolutions.LaptopRegistry.model.Laptop;
 import hu.maszeksolutions.LaptopRegistry.service.LaptopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,11 @@ public class LaptopRestController
     public Collection<Laptop> getLaptops()
     {
         return service.getAllLaptops();
+    }
+
+    @GetMapping(value = "Laptop/{serialNumber}")
+    public Laptop getLaptopBySerialNumber(@PathVariable String serialNumber) throws LaptopNotFound
+    {
+        return service.getLaptop(serialNumber);
     }
 }
