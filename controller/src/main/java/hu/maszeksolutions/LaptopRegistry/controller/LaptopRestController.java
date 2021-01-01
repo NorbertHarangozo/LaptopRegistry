05723f5d44524bcc79185f40088c3c6466901dc7
Norbert Harangozó <maszek.solutions@gmail.com>
2021-01-01 15:21:49 +0100

    [controller] Implement "parameterized listing of laptops" REST functionality.
    This is main reason why we needed the LaptopDAO refactor in the previous commit.

--- controller/src/main/java/hu/maszeksolutions/LaptopRegistry/controller/LaptopRestController.java
+++ controller/src/main/java/hu/maszeksolutions/LaptopRegistry/controller/LaptopRestController.java
@@ -1,14 +1,13 @@
 package hu.maszeksolutions.LaptopRegistry.controller;
 
 import hu.maszeksolutions.LaptopRegistry.exceptions.LaptopNotFound;
-import hu.maszeksolutions.LaptopRegistry.model.Laptop;
+import hu.maszeksolutions.LaptopRegistry.model.*;
 import hu.maszeksolutions.LaptopRegistry.service.LaptopService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 
 @RestController
@@ -19,9 +18,43 @@ public class LaptopRestController
     LaptopService service;
 
     @GetMapping(value = "Laptops")
-    public Collection<Laptop> getLaptops()
+    public Collection<Laptop> getLaptops(@RequestParam(required = false) Manufacturer manufacturer,
+                                         @RequestParam(required = false) String name,
+                                         @RequestParam(required = false) Color color,
+                                         @RequestParam(required = false) Double screenSize,
+                                         @RequestParam(required = false) PanelType panelType,
+                                         @RequestParam(required = false) Integer resolutionX,
+                                         @RequestParam(required = false) Integer resolutionY,
+                                         @RequestParam(required = false) Integer refreshRate,
+                                         @RequestParam(required = false) String cpu,
+                                         @RequestParam(required = false) MemoryType memoryType,
+                                         @RequestParam(required = false) Integer memorySize,
+                                         @RequestParam(required = false) String gpu,
+                                         @RequestParam(required = false) StorageType storageType,
+                                         @RequestParam(required = false) Integer storageSize,
+                                         @RequestParam(required = false) Boolean opticalDrive,
+                                         @RequestParam(required = false) Integer numberOfUSBPorts,
+                                         @RequestParam(required = false) String manufactureDate)
     {
-        return service.getAllLaptops();
+        LocalDate date = null;
+
+        if (manufactureDate != null)
+        {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+            try
+            {
+                date = LocalDate.parse(manufactureDate, formatter);
+            }
+            catch (Exception ignored)
+            { }
+        }
+
+        Collection<Laptop> laptops = service.getLaptopsByParam(manufacturer, name, color, screenSize, panelType,
+                                            resolutionX, resolutionY, refreshRate, cpu, memoryType, memorySize, gpu,
+                                            storageType, storageSize, opticalDrive, numberOfUSBPorts, date);
+
+        return laptops;
     }
 
     @GetMapping(value = "Laptop/{serialNumber}")
